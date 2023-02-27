d968f91682a979a1395c7025809785a5349e97f1
James Sgarella <Jimbo8702@gmail.com>
2023-02-27 15:55:38 -0500

    new photos added

--- upload.js
+++ upload.js
@@ -1,6 +1,6 @@
 const { uploadImages } = require("./helpers/upload.js");
 const fs = require("fs");
-let imagePaths = ["./images/image-0.jpeg", "images/image-1.jpeg"]; //fill this with image paths from the image folder
+let imagePaths = ["./images/image-0.jpeg", "./images/image-1.jpeg"]; //fill this with image paths from the image folder
 
 const upload = async () => {
   let locations = await uploadImages(imagePaths);
